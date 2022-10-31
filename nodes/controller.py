0c538557b2d38c53249cdae0108701f7c19288fa
Fskavlem <fredrik.skavlem@gmail.com>
2022-10-31 07:42:13 -0700

    bug fixes controller

--- nodes/controller.py
+++ nodes/controller.py
@@ -5,7 +5,9 @@ import math
 from math import sin, asin, cos, acos, tan, atan, atan2, exp, sqrt, pi
 from sensor_msgs.msg import Imu
 from sensor_msgs.msg import NavSatFix
-from std_msgs import Float32
+from std_msgs.msg import Float32
+from std_msgs.msg import Bool
+from geometry_msgs.msg import Point
 
 # TODO: Sensor readings are Float64, Thruster cmds are Float32. Verify that there are no issues
 class ControllerClass:
@@ -16,6 +18,8 @@ class ControllerClass:
         # Create a subscriber to the sensor topics using as a callback function to extract values into local variables
         self.sub_imu = rospy.Subscriber('/sensors/imu/data', Imu, self.clbk_imu, queue_size=1)
         self.sub_gps = rospy.Subscriber('/sensors/gps/fix', NavSatFix, self.clbk_gps, queue_size=1)
+        self.sub_auto = rospy.Subscriber('/controller/auto', Bool, self.clbk_auto, queue_size=1)
+        self.sub_sp = rospy.Subscriber('/controller/sp', Point, self.clbk_sp, queue_size=1)
         # TODO: Create topic for recieving lat/long setpoints
         
         # Create a publisher to the command topics
@@ -34,18 +38,21 @@ class ControllerClass:
         self.imu_az = 0
         self.gps_lat = 0
         self.gps_long = 0
+        self.auto = False
+        self.sp_lat = 0.0
+        self.sp_long = 0.0
         
         # setup global variables
         self.dt = 0.01          # sampling time
         self.I = 64.2           # angle between horizontal plane and magnetic vector
-        self.D = 18             # deviation between magnetic and geographic north
+        self.D = 0             # deviation between magnetic and geographic north
         self.B = 5.7065e-5      # magnetic field strength
         self.g = 9.81           # acceleration due to gravity
 
 
     #Callback function for the sensor topics
     def clbk_imu(self, msg):
-        self.imu_bx = msg.orientation.x                 # TODO: Verify values getting into variables
+        self.imu_bx = msg.orientation.x
         self.imu_by = msg.orientation.y
         self.imu_bz = msg.orientation.z
         self.imu_ax = msg.linear_acceleration.x
@@ -53,8 +60,15 @@ class ControllerClass:
         self.imu_az = msg.linear_acceleration.z
         
     def clbk_gps(self, msg):
-        self.gps_lat = msg.latitude                     # TODO: Verify values getting into variables
+        self.gps_lat = msg.latitude
         self.gps_long = msg.longitude
+    
+    def clbk_auto(self, msg):
+        self.auto = msg.data
+            
+    def clbk_sp(self, msg):
+        self.sp_lat = msg.x
+        self.sp_long = msg.y
 
     def FilterGPS(self, lat_prev, long_prev): # exponential filter
         tau = 0.1                                                   # TODO: move to global variable
@@ -81,9 +95,16 @@ class ControllerClass:
     def ProcessIMU(self,bx, by, bz, ax, ay, az):
         # TODO: zero angle need to be towards right. Range not important. can be [-inf,inf]
         pitch = asin(-ax/self.g)
-        roll = atan(ay/az)
-        yaw = atan((cos(pitch)*(bz*sin(roll) - by*cos(roll))) / (bx + self.B*sin(self.I)*sin(pitch))) + self.D
-        # yaw = atan(-by / bx) + self.D # simplified yaw calculations. Must disable roll and pitch
+        if az != 0:
+            roll = atan(ay/az)
+        else:
+            roll = pi
+        
+        if bx != 0:
+            #yaw = atan2((cos(pitch)*(bz*sin(roll) - by*cos(roll))), (bx + self.B*sin(self.I)*sin(pitch))) + self.D
+            yaw = atan2(-by, bx) + self.D*pi/180 # simplified yaw calculations. Must disable roll and pitch in gazebo
+        else:
+            yaw = pi
         
         return yaw
         
@@ -104,28 +125,25 @@ class ControllerClass:
         k = 1-exp(-self.dt/ptc)
         
         # setup controller memory variables
-        lat_prev, long_prev = 0
-        bx_prev, by_prev, bz_prev, ax_prev, ay_prev,az_prev = 0
-        ef_prev, df_prev = 0
+        lat_prev, long_prev = 0, 0
+        bx_prev, by_prev, bz_prev, ax_prev, ay_prev,az_prev = 0, 0, 0, 0, 0, 0
+        ef_prev, df_prev = 0, 0
         
-        # setpoints TODO: should be recieved in topic
-        lat_sp = 0
-        long_sp = 0
         
         n = 0
         while not finished: # TODO: add automatic mode from topic
             # get new filtered inputs
-            lat, long = self.FilterGPS(self, lat_prev, long_prev)
+            lat, long = self.FilterGPS(lat_prev, long_prev)
             lat_prev, long_prev = lat, long                                                                     # store previous values
-            bx,by,bz,ax,ay,az = self.FilterIMU(self, bx_prev, by_prev, bz_prev, ax_prev, ay_prev, az_prev)
+            bx,by,bz,ax,ay,az = self.FilterIMU(bx_prev, by_prev, bz_prev, ax_prev, ay_prev, az_prev)
             bx_prev, by_prev, bz_prev, ax_prev, ay_prev, az_prev = bx,by,bz,ax,ay,az                            # store previous values
             
             # process IMU values
-            theta = self.ProcessIMU(self, bx, by, bz, ax, ay, az)
+            theta = self.ProcessIMU(bx, by, bz, ax, ay, az)
             
             # calculate errors
-            d = max(sqrt((lat_sp-lat)^2+(long_sp-long)^2) - offset,0);      # dist is positive
-            angle = atan2((lat_sp-lat),(long_sp-long))                      # angle in [-pi,pi]
+            d = max(sqrt((self.sp_lat-lat)**2+(self.sp_long-long)**2) - offset,0);      # dist is positive
+            angle = atan2((self.sp_lat-lat),(self.sp_long-long))                      # angle in [-pi,pi]
             e = (angle-theta) % (2*pi)
             if e > pi:                                                      # error in [-pi,pi]
                 e = e - 2*pi
@@ -164,10 +182,12 @@ class ControllerClass:
                 wt = 0
 
             # Publish msg (scaled and inverted to [-1,1])
-            self.pub_wl.publish(max(min(wl/-5000,1),-1))
-            self.pub_wl.publish(max(min(wr/-5000,1),-1))
-            self.pub_wl.publish(max(min(wt/-6000,1),-1))
-            
+            #self.pub_wl.publish(max(min(wl/-5000,1),-1))
+            #self.pub_wr.publish(max(min(wr/-5000,1),-1))
+            #self.pub_wt.publish(max(min(wt/-6000,1),-1))
+            self.pub_wl.publish(sin(pi/4))
+            self.pub_wr.publish(theta*180/pi)
+            self.pub_wt.publish(e)
 
             # sleeps for the time needed to ensure that the loop will be executed with the previously defined frequency
             r.sleep()
